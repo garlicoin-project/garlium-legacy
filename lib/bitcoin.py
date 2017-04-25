1af213b99100ccd10fe6edf3eeb2638debcd3592
pooler <pooler@litecoinpool.org>
2017-04-25 16:17:22 +0200

    Litecoin: new P2SH address prefixes

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -40,7 +40,8 @@ import pyaes
 TESTNET = False
 NOLNET = False
 ADDRTYPE_P2PKH = 48
-ADDRTYPE_P2SH = 5
+ADDRTYPE_P2SH = 50
+ADDRTYPE_P2SH_ALT = 5
 ADDRTYPE_P2WPKH = 6
 XPRV_HEADER = 0x0488ade4
 XPUB_HEADER = 0x0488b21e
@@ -49,12 +50,13 @@ XPUB_HEADER_ALT = 0x019da462
 HEADERS_URL = "https://electrum-ltc.org/blockchain_headers"
 
 def set_testnet():
-    global ADDRTYPE_P2PKH, ADDRTYPE_P2SH, ADDRTYPE_P2WPKH
-    global XPRV_HEADER, XPUB_HEADER
+    global ADDRTYPE_P2PKH, ADDRTYPE_P2SH, ADDRTYPE_P2SH_ALT, ADDRTYPE_P2WPKH
+    global XPRV_HEADER, XPUB_HEADER, XPRV_HEADER_ALT, XPUB_HEADER_ALT
     global TESTNET, HEADERS_URL
     TESTNET = True
     ADDRTYPE_P2PKH = 111
-    ADDRTYPE_P2SH = 196
+    ADDRTYPE_P2SH = 58
+    ADDRTYPE_P2SH_ALT = 196
     ADDRTYPE_P2WPKH = 3
     XPRV_HEADER = 0x04358394
     XPUB_HEADER = 0x043587cf
@@ -437,7 +439,7 @@ def is_address(addr):
         addrtype, h = bc_address_to_hash_160(addr)
     except Exception:
         return False
-    if addrtype not in [ADDRTYPE_P2PKH, ADDRTYPE_P2SH]:
+    if addrtype not in [ADDRTYPE_P2PKH, ADDRTYPE_P2SH, ADDRTYPE_P2SH_ALT]:
         return False
     return addr == hash_160_to_bc_address(h, addrtype)
 
@@ -449,7 +451,7 @@ def is_p2pkh(addr):
 def is_p2sh(addr):
     if is_address(addr):
         addrtype, h = bc_address_to_hash_160(addr)
-        return addrtype == ADDRTYPE_P2SH
+        return addrtype in [ADDRTYPE_P2SH, ADDRTYPE_P2SH_ALT]
 
 def is_private_key(key):
     try:
